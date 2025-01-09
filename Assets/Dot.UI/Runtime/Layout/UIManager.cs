b601fdef9a5a2e2f5444496d69bf1faaaeaab8bb
Erick <liyubin724@163.com>
2025-01-09 18:48:57 +0800

    opt: 调整目录结构

---
+++ Assets/Dot.UI/Runtime/Layout/UIManager.cs
@@ -0,0 +1,26 @@
+﻿namespace DotEngine.UI
+{
+    public sealed class UIManager
+    {
+        private static UIManager instance;
+
+        public static UIManager CreateInstance()
+        {
+            if (instance == null)
+            {
+                instance = new UIManager();
+            }
+            return instance;
+        }
+
+        public static UIManager GetInstance()
+        {
+            return instance;
+        }
+
+        public static void DestroyInstance()
+        {
+            instance = null;
+        }
+    }
+}
